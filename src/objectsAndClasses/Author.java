409b752630c2827065043860899c84a09e1458fb
Dmitriy <jekas213@gmail.com>
2023-02-01 14:06:55 +0300

    готовое дз

---
+++ src/objectsAndClasses/Author.java
@@ -0,0 +1,25 @@
+package objectsAndClasses;
+
+public class Author {
+    private String firsName;
+    private String lastName;
+
+    public Author(String firsName, String lastName) {
+        this.firsName = firsName;
+        this.lastName = lastName;
+    }
+
+    public String getFirsName() {
+        return this.firsName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getFullName() {
+        return this.firsName + " " + this.lastName;
+    }
+
+
+}
